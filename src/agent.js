dd66a5929a5e934738c8697d9c70ef544390fc90
Ashutosh Behera <91386098+Ab6353@users.noreply.github.com>
2022-03-29 18:07:56 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = https://mern.ha.squareops.co.in;  //passed react application backend uri
+const API_ROOT = "https://mern.ha.squareops.co.in";  //passed react application backend uri
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
