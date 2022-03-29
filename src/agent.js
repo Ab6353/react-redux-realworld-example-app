fc42f6323041d577c6bb4a7ef0522244218c0eea
Ashutosh Behera <91386098+Ab6353@users.noreply.github.com>
2022-03-29 17:37:40 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -5,7 +5,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = process.env.REACT_APP_API;  //passed react application backend urii
+const API_ROOT = process.env.REACT_APP_API;  //passed react application backend uri
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
