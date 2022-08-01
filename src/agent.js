779dec430f96505136f7a8a92ff71fdab46beab7
Ashutosh Behera <91386098+Ashutosh-aws@users.noreply.github.com>
2022-08-01 16:14:35 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -4,7 +4,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT =  process.env.REACT_APP_API_ROOT;  //'http://abc/api'; 
+const API_ROOT =  process.env.REACT_APP_BACKEND_API;  //'http://abc/api'; 
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
